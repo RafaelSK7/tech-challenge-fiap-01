fc19b07a6f40cd07e919683144029dece08d5a7a
Jonathan <jonathanutsuni@gmail.com>
2025-04-29 21:59:49 -0300

    Convertendo getter and setters para lombok

--- src/main/java/fiap/tech/challenge/restaurant_manager/controllers/exceptions/ApiErrorArray.java
+++ src/main/java/fiap/tech/challenge/restaurant_manager/controllers/exceptions/ApiErrorArray.java
@@ -1,63 +1,21 @@
 package fiap.tech.challenge.restaurant_manager.controllers.exceptions;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ApiErrorArray {
-
     private LocalDateTime timestamp;
     private int status;
     private String error;
     private List<String> messages;
     private String path;
-
-    public ApiErrorArray(LocalDateTime timestamp, int status, String error, List<String> messages, String path) {
-        this.timestamp = timestamp;
-        this.status = status;
-        this.error = error;
-        this.messages = messages;
-        this.path = path;
-    }
-
-    // Getters e Setters
-
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(LocalDateTime timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public void setError(String error) {
-        this.error = error;
-    }
-
-    public List<String> getMessages() {
-        return messages;
-    }
-
-    public void setMessages(List<String> messages) {
-        this.messages = messages;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
-    }
 }

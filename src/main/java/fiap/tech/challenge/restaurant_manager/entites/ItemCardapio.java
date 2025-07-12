15092422c39b85b3f3b5c9374495ee5dad3c89ad
marcio72 <marcio@systemor.com.br>
2025-07-12 16:36:35 -0300

    ItensCardapio

---
+++ src/main/java/fiap/tech/challenge/restaurant_manager/entites/ItemCardapio.java
@@ -0,0 +1,16 @@
+package fiap.tech.challenge.restaurant_manager.entites;
+
+
+
+public class ItemCardapio {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private boolean localOnly;
+    private String photoPath;
+    
+}
+
+

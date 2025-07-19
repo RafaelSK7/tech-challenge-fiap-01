5fa44a9a0c02cb2feb7c6f11a29626c58974e94f
Felipe Nucci <flpnucci@gmail.com>
2025-07-19 17:58:13 -0300

    Chore: Organizando imports

--- src/main/java/fiap/tech/challenge/restaurant_manager/entites/response/LoginResponse.java
+++ src/main/java/fiap/tech/challenge/restaurant_manager/entites/response/LoginResponse.java
@@ -1,8 +1,6 @@
 package fiap.tech.challenge.restaurant_manager.entites.response;
 
 
-import fiap.tech.challenge.restaurant_manager.entites.UserType;
-
 public record LoginResponse(
         Long id,
         String name,

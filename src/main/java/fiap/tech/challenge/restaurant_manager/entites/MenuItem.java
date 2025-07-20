5d6c208cfa4233e57c13537d1c5038029b833fbc
marcio72 <marcio@systemor.com.br>
2025-07-20 00:19:44 -0300

    criação de records

--- src/main/java/fiap/tech/challenge/restaurant_manager/entites/MenuItem.java
+++ src/main/java/fiap/tech/challenge/restaurant_manager/entites/MenuItem.java
@@ -3,6 +3,7 @@ package fiap.tech.challenge.restaurant_manager.entites;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.*;
+import fiap.tech.challenge.restaurant_manager.entites.request.CreateMenuItemRequest;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 
@@ -41,4 +42,17 @@ public class MenuItem {
 
     @Column(name = "photoPath")
     private String photoPath;    
+ 
+    @JoinColumn(name= "restaurants_id", referencedColumnName = "id")
+    @ManyToOne()
+    private Restaurant restaurant;
+
+    public MenuItem ( CreateMenuItemRequest createMenuItemRequest, Restaurant restaurant){
+        this.name = createMenuItemRequest.name();
+        this.description = createMenuItemRequest.description();
+        this.price = createMenuItemRequest.price();
+        this.localOnly = createMenuItemRequest.localOnly();
+        this.photoPath = createMenuItemRequest.photoPath();
+        this.restaurant = restaurant;
+    } 
 }
--- src/main/java/fiap/tech/challenge/restaurant_manager/entites/request/CreateMenuItemRequest.java
+++ src/main/java/fiap/tech/challenge/restaurant_manager/entites/request/CreateMenuItemRequest.java
@@ -8,6 +8,4 @@ public record CreateMenuItemRequest(
     String photoPath
 ) {
 
-
-
 }
